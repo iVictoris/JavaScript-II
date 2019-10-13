aa5224c31f5ff80409c51eceb6380eb9f18cfc4f
Victor Tran <victor.n.tran@outlook.com>
2019-10-12 19:58:48 -0700

    complete callbacks and stretch

--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -1,6 +1,10 @@
-// Create a higher order function and invoke the callback function to test your work. You have been provided an example of a problem and a solution to see how this works with our items array.  Study both the problem and the solution to figure out the rest of the problems.
+// Create a higher order function
+// and invoke the callback function to test your work.
+// You have been provided an example of a problem and a solution to see how this
+// works with our items array.
+// Study both the problem and the solution to figure out the rest of the problems.
 
-const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
+const items = ["Pencil", "Notebook", "yo-yo", "Gum"];
 
 /* 
 
@@ -38,26 +42,32 @@ const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
   console.log(test2); // "this Pencil is worth a million dollars!"
 */
 
-
 function getLength(arr, cb) {
+  // normally we would check if there was a length property
+  return cb(arr.length);
   // getLength passes the length of the array into the callback.
 }
 
 function last(arr, cb) {
   // last passes the last item of the array into the callback.
+  return cb(arr[arr.length - 1]);
 }
 
 function sumNums(x, y, cb) {
   // sumNums adds two numbers (x, y) and passes the result to the callback.
+  return cb(x + y);
 }
 
 function multiplyNums(x, y, cb) {
   // multiplyNums multiplies two numbers and passes the result to the callback.
+  return cb(x * y);
 }
 
 function contains(item, list, cb) {
   // contains checks if an item is present inside of the given array/list.
   // Pass true to the callback if it is, otherwise pass false.
+  const itemExists = list.filter(i => item === i).length;
+  return itemExists ? cb(true) : cb(false);
 }
 
 /* STRETCH PROBLEM */
@@ -66,4 +76,13 @@ function removeDuplicates(array, cb) {
   // removeDuplicates removes all duplicate values from the given array.
   // Pass the duplicate free array to the callback function.
   // Do not mutate the original array.
+  const arrayValues = Object.values([1, 2, 3, 4, 1, 2, 3, 4]);
+
+  const arrayObject = arrayValues.reduce((obj, val) => {
+    obj[val] = true;
+    return obj
+  }, {});
+
+  const removedDups = Object.keys(arrayObject)
+  return cb(removedDups);
 }
